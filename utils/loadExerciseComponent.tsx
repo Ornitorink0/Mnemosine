3852f56725f6c0468ad81cf915e2c2ea28fa1394
Ornitorink0 <matteogurri1@gmail.com>
2025-05-27 09:37:30 +0200

    Aggiunta la funzione loadExerciseComponent per il caricamento dinamico degli esercizi

---
+++ utils/loadExerciseComponent.tsx
@@ -0,0 +1,8 @@
+import dynamic from "next/dynamic";
+
+export function loadExerciseComponent(exerciseId: string | number) {
+  return dynamic(() => import(`@/components/exercises/${exerciseId}`), {
+    ssr: false,
+    loading: () => <p>Caricamento dell&apos;esercizio...</p>,
+  });
+}
