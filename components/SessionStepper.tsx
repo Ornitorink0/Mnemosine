e8dd46fd278e7bf3b9653f53f170e40b915da1d4
Ornitorink0 <matteogurri1@gmail.com>
2025-05-27 09:36:17 +0200

    Aggiunto il componente SessionStepperClient per gestire dinamicamente gli esercizi in una sessione

---
+++ components/SessionStepper.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Stepper, { Step } from "./stepper";
+import { loadExerciseComponent } from "@/utils/loadExerciseComponent";
+
+type Exercise = {
+  exerciseId: string;
+  description: string;
+  timeSpent: number;
+  nErrors: number;
+};
+
+type Props = {
+  exercises: Exercise[];
+};
+
+export default function SessionStepperClient({ exercises }: Props) {
+  return (
+    <Stepper
+      initialStep={0}
+      onStepChange={(step) => console.log("Step changed:", step)}
+      onFinalStepCompleted={() => alert("Sessione completata!")}
+      backButtonText="Indietro"
+      nextButtonText="Avanti"
+    >
+      {exercises.map((exercise) => {
+        const DynamicExercise = loadExerciseComponent(exercise.exerciseId);
+        return (
+          <Step key={exercise.exerciseId}>
+            <div className="p-6">
+              <h2 className="text-xl font-bold mb-4">
+                Esercizio {exercise.exerciseId}: {exercise.description}
+              </h2>
+              <DynamicExercise />
+            </div>
+          </Step>
+        );
+      })}
+    </Stepper>
+  );
+}
