b6dcd0670661a52bd1ffee8de6f93dc81b1ed4e3
Ornitorink0 <ornitorink0.dev@gmail.com>
2025-06-07 17:47:22 +0200

    Formattato

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from 'next';
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-module.exports = withBundleAnalyzer({});const nextConfig: NextConfig = {
+module.exports = withBundleAnalyzer({});
+const nextConfig: NextConfig = {
   /* config options here */
 };
 
 export default nextConfig;
-
